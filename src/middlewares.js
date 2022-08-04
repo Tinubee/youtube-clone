a6a902d0f776b30fa3733d5a4e640bf5ced7689a
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-08-04 17:20:15 +0900

    Fixed Video bug(usersave)
    :wq

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,7 +3,6 @@ import multer from "multer";
 export const localsMiddleware = (req, res, next) => {
   res.locals.isLoggedIn = Boolean(req.session.loggedIn);
   res.locals.user = req.session.user || {};
-  console.log(res.locals);
   next();
 };
 
