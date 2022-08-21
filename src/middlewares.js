4175254704e6bd4b976362ff2b876762335c098b
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-08-21 21:44:04 +0900

    isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,15 +9,24 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "tinubee-youtube",
+  bucket: "tinubee-youtube/images",
+  acl: "public-read",
+});
+
+const s3VidoeUploader = multerS3({
+  s3: s3,
+  bucket: "tinubee-youtube/videos",
   acl: "public-read",
 });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.isLoggedIn = Boolean(req.session.loggedIn);
   res.locals.user = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -44,12 +53,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 20000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3VidoeUploader : undefined,
 });
